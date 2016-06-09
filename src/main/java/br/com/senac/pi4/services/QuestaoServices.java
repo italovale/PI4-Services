129df47be78dd6309980247c58075288ae7fb92c
italovale <vale.italo@gmail.com>
2016-06-09 20:28:32 -0300

    aeee

--- src/main/java/br/com/senac/pi4/services/QuestaoServices.java
+++ src/main/java/br/com/senac/pi4/services/QuestaoServices.java
@@ -49,7 +49,8 @@ public class QuestaoServices {
 		try {
 			conn = DatabaseUtil.get().conn();
 
-			psta = conn.prepareStatement("select q.codQuestao, q.textoQuestao, q.codTipoQuestao, qe.tempo, a.codAlternativa, a.textoAlternativa from	questao q inner join QuestaoEvento qe on q.codQuestao = qe.codQuestao left join	Alternativa a on q.codQuestao = a.codQuestao where	ativo = 1 and codstatus = 'C' and codevento = ?");
+			psta = conn.prepareStatement("select q.codQuestao, q.textoQuestao, q.codTipoQuestao, qe.tempo, a.codAlternativa, a.textoAlternativa from questao q inner join QuestaoEvento qe on q.codQuestao = qe.codQuestao left join	Alternativa a on q.codQuestao = a.codQuestao where	ativo = 1 and codstatus = 'A' and codevento = ?");
+			//psta = conn.prepareStatement("select q.codQuestao, q.textoQuestao, q.codTipoQuestao, qe.tempo, a.codAlternativa, a.textoAlternativa from	questao q inner join QuestaoEvento qe on q.codQuestao = qe.codQuestao left join	Alternativa a on q.codQuestao = a.codQuestao where codstatus = 'A' and codevento = ?");
 			psta.setInt(1, eventoId);
 			
 			
@@ -63,7 +64,7 @@ public class QuestaoServices {
 				questao.setTextoQuestao(rs.getString("textoQuestao"));
 				questao.setCodTipoQuestao(rs.getString("codTipoQuestao"));
 				
-				if(questao.getCodTipoQuestao().equalsIgnoreCase("A") || questao.getCodTipoQuestao().equalsIgnoreCase("V"))
+				if(questao.getCodTipoQuestao().equalsIgnoreCase("A"))
 				{
 					Alternativa alter = new Alternativa();
 					alter.setCodAlternativa(rs.getInt("codAlternativa"));
