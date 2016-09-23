691edae04de3eff4ce6dd2cdedf142efdccbf87c
italovale <vale.italo@gmail.com>
2016-09-23 14:57:09 -0300

    novos serviços de grupos
    - Criar no grupo
    - Entrar no grupo

--- src/main/java/br/com/senac/pi4/services/GrupoServices.java
+++ src/main/java/br/com/senac/pi4/services/GrupoServices.java
@@ -17,10 +17,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import br.com.senac.pi4.entity.ParticipanteGrupo;
+import br.com.senac.pi4.entity.Resposta;
 import br.com.senac.pi4.util.DatabaseUtil;
 
 import com.google.gson.Gson;
- //https://github.com/jpardogo/PagerSlidingTabStrip
 @Path("/grupo")
 public class GrupoServices {
  
@@ -62,6 +62,8 @@ public class GrupoServices {
 		}
 		return listPg;
 	}
+
+	
 	
 	@GET
 	@Path("/{param}")
@@ -91,6 +93,96 @@ public class GrupoServices {
 		String jsonUser = gson.toJson(user);
 		System.out.println("salvando usuario "+jsonUser);
 		return Response.status(200).entity("").build();
+ 
+	}
+	
+	
+	@GET
+	@Path("/{param}/{param1}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response setGrupo(@PathParam("param") Integer idGrupo, @PathParam("param1") Integer idParticiapante) {
+		Boolean result = false;
+		
+		try {
+			
+			int rowChange = 0;
+			Connection conn = null;
+		
+			try {
+				conn = DatabaseUtil.get().conn();		
+				
+				PreparedStatement stmt = conn.prepareStatement("insert into ParticipanteGrupo(codGrupo, CodParticiapante) values(?, ?)");
+				stmt.setInt(1, idGrupo);
+				stmt.setInt(2, idParticiapante);
+		        //rowChange = stmt.executeUpdate();
+				
+				if(rowChange > 0)
+				{
+					result = true;
+				}
+				
+			} catch (SQLException e) {
+				throw e;
+			} catch (Exception e) {
+				throw e;
+			} finally {
+				if (conn != null)
+					conn.close ();
+			}
+			
+		} catch (Exception e) {
+			return Response.status(500).entity(null).build();	
+		}
+		if (!result)
+			return Response.status(404).entity(null).build();
+		
+		
+		return Response.status(200).entity(result).build();
+ 
+	}
+	
+	@GET
+	@Path("/{param}/{param1}/{param2}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response setGrupo(@PathParam("param") Integer idEvento, @PathParam("param1") Integer idParticiapante, @PathParam("param2") String nmGrupo) {
+		Boolean result = false;
+		
+		try {
+			
+			int rowChange = 0;
+			Connection conn = null;
+		
+			try {
+				conn = DatabaseUtil.get().conn();		
+				
+				PreparedStatement stmt = conn.prepareStatement("insert into grupo(codEvento, nmGrupo, codLider)values(?, ?, ?)");
+				stmt.setInt(1, idEvento);
+				stmt.setInt(2, idParticiapante);
+				stmt.setString(3, nmGrupo);
+		        //rowChange = stmt.executeUpdate();
+				
+				if(rowChange > 0)
+				{
+					result = true;
+				}
+				
+			} catch (SQLException e) {
+				throw e;
+			} catch (Exception e) {
+				throw e;
+			} finally {
+				if (conn != null)
+					conn.close ();
+			}
+			
+		} catch (Exception e) {
+			return Response.status(500).entity(null).build();	
+		}
+		if (!result)
+			return Response.status(404).entity(null).build();
+		
+		
+		return Response.status(200).entity(result).build();
  
 	}
 	
